2dbc42668c02b2a40b9649c80464064c5c069a71
djordjereact <djole07mihailovic@gmail.com>
2020-05-07 18:22:28 +0200

    I have finished with searching in hooks app

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-
+import React, { useState, useEffect, useCallback } from 'react';
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
 import Search from './Search';
@@ -7,18 +6,34 @@ import Search from './Search';
 const Ingredients = () => {
     const [userIngredients, setUserIngredients] = useState([]);
 
+    useEffect(() => {
+        console.log('RENDERING INGREDIENTS', userIngredients);
+    }, [userIngredients]);
+
+    const filteredIngredientsHandler = useCallback(filteredIngredients => {
+        setUserIngredients(filteredIngredients);
+    }, []);
+
     const addIngredientHandler = ingredient => {
-        setUserIngredients(prevIngredients => [
-            ...prevIngredients,
-            {id: Math.random().toString(), ...ingredient}
-        ]);
+        fetch('https://react-hooks-update-fb7bf.firebaseio.com/ingredients.json', {
+            method: 'POST',
+            body: JSON.stringify(ingredient),
+            headers: { 'Content-type': 'application/json' }
+        }).then(response => {
+            return response.json();
+        }).then(responseData => {
+            setUserIngredients(prevIngredients => [
+                ...prevIngredients,
+                {id: responseData.name, ...ingredient}
+            ])
+        });
     };
 
   return (
     <div className="App">
       <IngredientForm onAddIngredient={addIngredientHandler} />
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={() => {}}/>
       </section>
     </div>
